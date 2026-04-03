9e4a35397d1fa9f2abd6eb70606d8f8a7aeddd57
Neo6424 <129708429+Neo6424@users.noreply.github.com>
2026-04-03 09:07:17 +0900

    Update 1-1.c

---
+++ code/1-1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void double_elements(int *arr, int num){
+  arr[0] = arr[0]+arr[0];
+  arr[1] = arr[1]+arr[1];
+  arr[2] = arr[2]+arr[2];
+  arr[3] = arr[3]+arr[3];
+  arr[4] = arr[4]+arr[4];
+}
+
+int main() {
+  int arr[5];
+
+  scanf("%d %d %d %d %d", &arr[0], &arr[1], &arr[2], &arr[3], &arr[4]);
+
+  double_elements(arr,5);
+  for(int i=0; i<5; i++){
+    printf("%d\n",arr[i]);
+  }
+    
+  return 0;
+}
