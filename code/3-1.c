fcae7e596cb2d3ccc92158933ab9ec3037a5b026
Neo6424 <129708429+Neo6424@users.noreply.github.com>
2026-04-03 10:03:26 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -3,7 +3,9 @@ void read_only(const int *arr,int size){
 
   for (int i = 0; i <size; i++){
     printf("%d ", *(arr +i));
-    printf("\n");
+    if ( i <size-1){
+      printf("\n");
+    }
   }
 }
 
